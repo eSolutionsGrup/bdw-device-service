17b52620feade7db10496afda080140ed69a806f
vborcea <virgil@esolutions.ro>
2021-10-14 08:24:05 +0300

    * message package

--- src/main/kotlin/ro/esolutions/bdw/message/MessageController.kt
+++ src/main/kotlin/ro/esolutions/bdw/message/MessageController.kt
@@ -1,4 +1,4 @@
-package ro.esolutions.bdw.user
+package ro.esolutions.bdw.message
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
